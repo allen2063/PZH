45a0b7d9fd0915f37a65d33130e1280c986ec2fc
allen2063 <199128927@qq.com>
2015-07-01 17:17:05 +0800

    Initial Commit

---
+++ PZH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZH
+//
+//  Created by ZengYifei on 15/7/1.
+//  Copyright (c) 2015年 IOS-developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PZH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZH
+//
+//  Created by ZengYifei on 15/7/1.
+//  Copyright (c) 2015年 IOS-developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
